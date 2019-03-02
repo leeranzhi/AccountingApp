83b187f2acf91a05be22614d4504fb5961a5dcec
leecode <2804581329@qq.com>
2019-03-02 11:33:15 +0800

    实现键盘逻辑约束用户输入.

--- app/src/main/java/com/leecode1988/accountingapp/AddRecordActivity.java
+++ app/src/main/java/com/leecode1988/accountingapp/AddRecordActivity.java
@@ -7,17 +7,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AddRecordActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "AddRecordActivity";
+    private String userInput = "";
+    private TextView amountText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_record);
 
+        initView();
+    }
 
+    private void initView() {
         findViewById(R.id.keyboard_one).setOnClickListener(this);
         findViewById(R.id.keyboard_two).setOnClickListener(this);
         findViewById(R.id.keyboard_three).setOnClickListener(this);
@@ -29,6 +35,7 @@ public class AddRecordActivity extends AppCompatActivity implements View.OnClick
         findViewById(R.id.keyboard_nine).setOnClickListener(this);
         findViewById(R.id.keyboard_zero).setOnClickListener(this);
 
+        amountText = findViewById(R.id.textView_addView_amount);
         handleDot();
         handleBackspace();
         handleDone();
@@ -41,6 +48,11 @@ public class AddRecordActivity extends AppCompatActivity implements View.OnClick
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "Dot: ");
+
+                if (!userInput.contains(".")) {
+                    userInput += ".";
+
+                }
             }
         });
     }
@@ -58,7 +70,16 @@ public class AddRecordActivity extends AppCompatActivity implements View.OnClick
         findViewById(R.id.keyboard_backspace).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "BackSpace: ");
+
+                if (userInput.length() > 0) {
+                    userInput = userInput.substring(0, userInput.length() - 1);
+                }
+
+                if (userInput.length() > 0 && userInput.charAt(userInput.length() - 1) == '.') {
+                    userInput = userInput.substring(0, userInput.length() - 1);
+
+                }
+                updateAmountText();
             }
         });
     }
@@ -67,7 +88,12 @@ public class AddRecordActivity extends AppCompatActivity implements View.OnClick
         findViewById(R.id.keyboard_done).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "Done: ");
+                if (!userInput.equals("")) {
+                    double amount = Double.valueOf(userInput);
+                    Log.d(TAG, "final amount is " + amount);
+                } else {
+                    
+                }
             }
         });
     }
@@ -79,6 +105,41 @@ public class AddRecordActivity extends AppCompatActivity implements View.OnClick
 
         Log.d(TAG, input);
 
+        if (userInput.contains(".")) {
+            //小数点后两位数,
+            // 分割.前后的位数
+            if (userInput.split("\\.").length == 1 || userInput.split("\\.")[1].length() < 2) {
+                userInput += input;
+            }
+        } else {
+            userInput += input;
+        }
+
+        updateAmountText();
+    }
+
+    private void updateAmountText() {
+        Log.d(TAG, "userInput is " + userInput);
+
+        if (userInput.contains(".")) {
+
+            if (userInput.split("\\.").length == 1) {
+                amountText.setText(userInput + "00");
+            } else if (userInput.split("\\.")[1].length() == 1) {
+                amountText.setText(userInput + "0");
+            } else if (userInput.split("\\.")[1].length() == 2) {
+                amountText.setText(userInput);
+            }
+
+        } else {
+            //""
+            if (userInput.equals("")) {
+                amountText.setText("0.00");
+            } else {
+                amountText.setText(userInput + ".00");
+            }
+
+        }
     }
 
     public static void actionStart(Context context, String data) {
